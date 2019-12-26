317c14e28f4c014def8fe63117d98c2936c8d17a
PENGLIU <15641188595@qq.com>
2019-12-26 10:48:39 +0800

    v1

--- java/thread/src/com/liupeng/test1.java
+++ java/thread/src/com/liupeng/test1.java
@@ -2,10 +2,38 @@ package com.liupeng;
 
 /**
  * Package: com.liupeng
- * Description：
+ * Description：java volatile 可见性的验证
  * Author: liupissc@cn.ibm.com
  * Date:  12/26/2019 9:58 AM
  * Modified By:
+ *
  */
+import java.util.concurrent.*;
+class MyData{
+    //private int number=0;
+    private volatile int number=0;
+    public void addTo60(){
+        this.number=60;
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+}
 public class test1 {
+    public static void main(String[] args) {
+        MyData myData=new MyData();
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName()+"\tcome in");
+            try{TimeUnit.SECONDS.sleep(3);} catch(Exception e){e.printStackTrace();}
+            myData.addTo60();
+        },"AAA").start();
+
+        while(myData.getNumber()==0){
+
+        }
+        System.out.println(Thread.currentThread().getName()+"\tmisson is over");
+    }
+
 }
